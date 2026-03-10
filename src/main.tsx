f58241ba1700bbbb56899c16905300f4b1129230
jirateep12z <70580937+jirateep12z@users.noreply.github.com>
2026-03-10 10:05:31 +0700

    feat(*): initial commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { ConfigBuilderApp } from '@/features/config-builder';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('Root element not found');
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <ConfigBuilderApp />
+  </StrictMode>
+);
